4e8eb3271ace59abe1c9176e364ac60d58e8e9a8
sayudhalw <sayudhawebdesign@hotmail.com>
2019-08-10 16:59:36 +0700

    configuring wds thorugh webpack config

--- src/component.js
+++ src/component.js
@@ -1,4 +1,4 @@
-export default (text = 'Hello World') => {
+export default (text = 'Hello Ghaeda') => {
     const element = document.createElement('div');
 
     element.innerHTML = text;
--- webpack.config.js
+++ webpack.config.js
@@ -7,3 +7,23 @@ module.exports = {
         })
     ]
 }
+
+module.exports = {
+    devServer: {
+        // display only errors to reduce the amount of output
+        stats: "errors-only",
+
+        // Parse host and port from env to allow customization
+        // 
+        // If you use Docker, Vagrant or CLoud9, set 
+        // host: "0.0.0.0";
+        // 
+        // 0.0.0.0 is available to all network devices
+        // unlike default 'localhost'.
+
+        host: process.env.HOST, // Defaults to localhost
+        port: process.env.PORT, // Defaults to 8080
+
+        open: true, // Open the page in browser
+    },
+}
